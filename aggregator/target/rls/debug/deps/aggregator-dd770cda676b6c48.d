38d72682d2c3a456e96076f3e313656458a10b1e
chansuke <chansuke@georepublic.de>
2018-02-11 20:48:43 +0900

    Add directory

---
+++ aggregator/target/rls/debug/deps/aggregator-dd770cda676b6c48.d
@@ -0,0 +1,5 @@
+/Users/YusukeAbe/code/rust/rust_study/aggregator/target/rls/debug/deps/aggregator-dd770cda676b6c48.rmeta: src/lib.rs
+
+/Users/YusukeAbe/code/rust/rust_study/aggregator/target/rls/debug/deps/aggregator-dd770cda676b6c48.d: src/lib.rs
+
+src/lib.rs:
---
+++ communicator/target/rls/debug/deps/communicator-3450e7c283a794a1.d
@@ -0,0 +1,7 @@
+/Users/YusukeAbe/code/rust/rust_study/communicator/target/rls/debug/deps/communicator-3450e7c283a794a1.rmeta: src/lib.rs src/client.rs src/network/mod.rs
+
+/Users/YusukeAbe/code/rust/rust_study/communicator/target/rls/debug/deps/communicator-3450e7c283a794a1.d: src/lib.rs src/client.rs src/network/mod.rs
+
+src/lib.rs:
+src/client.rs:
+src/network/mod.rs:
