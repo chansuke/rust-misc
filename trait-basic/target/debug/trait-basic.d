00387951c1b11e01ba86e835930e6149aa344041
chansuke <chansuke@georepublic.de>
2019-04-30 01:48:16 +0900

    a

---
+++ trait-basic/target/debug/trait-basic.d
@@ -0,0 +1,1 @@
+/Users/YusukeAbe/code/rust/rust-study/trait-basic/target/debug/trait-basic.dSYM: /Users/YusukeAbe/code/rust/rust-study/trait-basic/src/main.rs
---
+++ trait-basic/target/rls/debug/deps/trait_basic-994c77ccc0e3a898.d
@@ -0,0 +1,5 @@
+/Users/YusukeAbe/code/rust/rust-study/trait-basic/target/rls/debug/deps/trait_basic-994c77ccc0e3a898.rmeta: src/main.rs
+
+/Users/YusukeAbe/code/rust/rust-study/trait-basic/target/rls/debug/deps/trait_basic-994c77ccc0e3a898.d: src/main.rs
+
+src/main.rs:
