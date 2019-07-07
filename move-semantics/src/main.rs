99b9ca27cf30239cbacc2e006e3d5f3514b9f66f
chansuke <chansuke@georepublic.de>
2019-07-07 13:26:07 +0900

    Update move semantics

--- move-semantics/src/main.rs
+++ move-semantics/src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 struct Parent(usize, Child, Child);
 
 use std::ops::Drop;
@@ -9,9 +9,11 @@ impl Drop for Parent {
     }
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 struct Child(usize);
 
+//https://doc.rust-lang.org/std/ops/trait.Drop.html
+// dropメソッドは値が破棄される直前に呼ばれる
 impl Drop for Child {
     fn drop(&mut self) {
         println!("Dropping {:?}", self);
@@ -19,20 +21,19 @@ impl Drop for Child {
 }
 
 fn main() {
-    println!("Move semantics!!");
+    println!("ムーベセマンティックス:");
     move_semantics();
-    println!("\nBorrow: ");
+    println!("\nBorrow:");
     borrow();
 }
 
 fn move_semantics() {
     let mut p1 = Parent(1, Child(11), Child(12));
-    let p2 = p1; //所有権の移転
+    let p2 = p1;
     println!("p2: {:?}", p2);
-    //println!("p1: {:?}", p1); //これはエラーになる
 
-    p1 = Parent(2, Child(21), Child(22));
-    println!("p1: {:?}", p1);
+    p1 = Parent(2, Child(21), Child(22)); // p1を別の値に束縛する
+    println!("p1: {:?}", p1); // p1は別の値の所有権を持つためアクセスできる
 }
 
 fn f1(p: &Parent) {
@@ -40,12 +41,12 @@ fn f1(p: &Parent) {
 }
 
 fn f2(p: &mut Parent) {
-    p.0 *= 1;
+    p.0 *= 2;
 }
 
 fn borrow() {
     let mut p1 = Parent(1, Child(11), Child(12));
     f1(&p1);                   // f1には所有権をムーブせず、不変の参照を渡す
     f2(&mut p1);               // f2には所有権をムーブせず、可変の参照を渡す
-    println!("p1: {:?}", p1); 
+    println!("p1: {:?}", p1);  // p1は値の所有権を失っていないのでアクセスできる
 }
