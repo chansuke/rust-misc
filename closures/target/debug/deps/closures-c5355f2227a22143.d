6eff09ea2030ffbcacb57dbd60fb344820843c7e
chansuke <chansuke@georepublic.de>
2019-07-21 23:54:58 +0900

    a

---
+++ closures/target/debug/deps/closures-c5355f2227a22143.d
@@ -0,0 +1,5 @@
+/Users/YusukeAbe/code/rust/rust-study/closures/target/debug/deps/closures-c5355f2227a22143: src/main.rs
+
+/Users/YusukeAbe/code/rust/rust-study/closures/target/debug/deps/closures-c5355f2227a22143.d: src/main.rs
+
+src/main.rs:
