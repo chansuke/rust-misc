a0fae69b11e82627f4936f33ac0525cd458a211c
chansuke <chansuke@georepublic.de>
2019-10-26 00:25:53 +0900

    a

---
+++ image-handler/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let files = Path::new("./files");
+    fs::create_dir(files).ok();
+    let addr = ([127, 0, 0, 1], 8080).into();
+    let builder = Server::bind(&addr);
+    let server = builder.serve(move || {
+        service_fn(move |req| microservice_handler(req, &files))
+    });
+    let server = server.map_err(drop);
+    hyper::rt::run(server);
+}
