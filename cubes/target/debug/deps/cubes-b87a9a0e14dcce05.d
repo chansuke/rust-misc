a8adfa4f8eb480ab3c89d84ae2588de147508d4b
chansuke <chansuke@georepublic.de>
2020-01-18 21:44:48 +0900

    Add an another miscs

---
+++ cubes/target/debug/deps/cubes-b87a9a0e14dcce05.d
@@ -0,0 +1,5 @@
+/Users/YusukeAbe/code/rust/rust-misc/cubes/target/debug/deps/cubes-b87a9a0e14dcce05: src/main.rs
+
+/Users/YusukeAbe/code/rust/rust-misc/cubes/target/debug/deps/cubes-b87a9a0e14dcce05.d: src/main.rs
+
+src/main.rs:
---
+++ cubes/target/rls/debug/deps/cubes-14ca82b56960624d.d
@@ -0,0 +1,5 @@
+/Users/YusukeAbe/code/rust/rust-misc/cubes/target/rls/debug/deps/cubes-14ca82b56960624d.rmeta: src/main.rs
+
+/Users/YusukeAbe/code/rust/rust-misc/cubes/target/rls/debug/deps/cubes-14ca82b56960624d.d: src/main.rs
+
+src/main.rs:
